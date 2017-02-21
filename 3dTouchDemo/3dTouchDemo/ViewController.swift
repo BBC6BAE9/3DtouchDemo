860a6501d0f1f832cbb856937de2725505999858
BBC6BAE9 <chinahuanghong@gmail.com>
2017-02-21 10:51:56 +0800

    3dTouchDemo

---
+++ 3dTouchDemo/3dTouchDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  3dTouchDemo
+//
+//  Created by BBC6BAE9 on 2017/2/21.
+//  Copyright © 2017年 BBC6BAE9. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
